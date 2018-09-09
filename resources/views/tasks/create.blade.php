9f30e253aee0c8efb0a199d6f36d9c357ee8d252
nobu.korekara <nobu.ootuki+job@gmail.com>
2018-09-09 07:11:39 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,15 +4,23 @@
 
 <h1>タスクリスト新規作成ページ</h1>
 
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-        
-        {!! Form::label('content', '内容:') !!}
-        {!! Form::text('content') !!}
 
-        {!! Form::submit('投稿') !!}
-
+ <div class="row">
+   <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-3 ">
+      {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    
+    
+      <div class="form-group">
+        {!! Form::label('status', 'タスク:') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+      </div> 
+        <div class="form-group">
+        {!! Form::label('content', 'ステータス:') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+       </div>
+        {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+   
     {!! Form::close() !!}
-
+   </div>
+  </div>
 @endsection
